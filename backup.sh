fec85493d84fbb93c08fcfa030073f3368a4a59e
jesseik <jesse.ikola@jubic.fi>
2016-10-07 10:28:55 +0300

    Changes to backup script

--- backup.sh
+++ backup.sh
@@ -3,32 +3,12 @@
 datadir='/volume/mysql_data/backups/'
 while [ 1 ]
 do
-    #sleep $[ ( $RANDOM % 24 )  + 1 ]h
-    sleep 5s
+    sleep $[ ( $RANDOM % 24 )  + 1 ]h
     backups=`ls -1 $datadir |wc -l`
-    echo $backups
     echo $DATABASE_BACKUPS_MAX
     if [ "$backups" -gt "$DATABASE_BACKUPS_MAX" ]; then
       echo "deleting"
       find $datadir -mmin "+$DATABASE_BACKUPS_MAX" -type f -delete
-      # Poistetaan vanhin
-      #declare -a FILELIST
-      #echo $datadir
-      #backup_files=$datadir*
-      #for f in $datadir*; do
-        #echo $f
-        #filedate=${f:0:16}
-        #if [ -z "$oldest_backup"]; then
-        #  oldest_backup=filedate
-        #else
-        #  if [ $oldest_backup \> $filedate ]; then
-        #    oldest_backup=$filedate
-        ##  fi
-        #  echo "else"
-        #fi
-      #done
-      #echo "delete $(oldest_backup)"
-      #rm -fv $oldest_backup
     fi
-    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases > /volume/mysql_data/backups/$(date +%y%m%d%s)-mysql-backup.sql
+    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases > /volume/mysql_data/backups/$(date +%y%m%d)-mysql-backup.sql
 done
