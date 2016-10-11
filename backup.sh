f88ff9ce9afb9fd653f775dda4e1d5ed169e405b
jesseik <jesse.ikola@jubic.fi>
2016-10-11 09:47:17 +0300

    Chages to backup script

--- backup.sh
+++ backup.sh
@@ -10,5 +10,6 @@ do
       echo "deleting"
       find $datadir -mmin "+$DATABASE_BACKUPS_MAX" -type f -delete
     fi
+    if []
     mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --all-databases > /volume/mysql_data/backups/$(date +%y%m%d)-mysql-backup.sql
 done
